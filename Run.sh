49e10d7143ec90fb86cb4a3735523d3c256cb013
Bhavesh <107500953+bhavesh-21@users.noreply.github.com>
2024-05-20 09:16:31 +0530

    f

--- Run.sh
+++ Run.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
-echo "Starting Server..............................." 
-if [ -d ".env" ];
-then
-    echo "Enabling virtual env"
-else
-    echo "No Virtual env. Please run Run.sh"
-    exit N
-fi
+# echo "Starting Server..............................." 
+# if [ -d ".env" ];
+# then
+#     echo "Enabling virtual env"
+# else
+#     echo "No Virtual env. Please run Run.sh"
+#     exit N
+# fi
 
-. .env/Scripts/activate
-export ENV=development
-python app.py
-deactivate
+# . .env/Scripts/activate
+# export ENV=development
+# python app.py
+# deactivate
+
+
+celery -A app:celery worker -l INFO &
+wait 5
+
+gunicorn app:app
